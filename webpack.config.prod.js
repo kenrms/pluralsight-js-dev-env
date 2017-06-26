cf8baca3cf1261aeedd7f310c781209510a03e21
Kenneth Ramos <kramos@eduware.com>
2017-06-26 18:31:45 -0400

    configs for cache busting, and html templating with ejs for use with error logging config

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,6 +1,8 @@
 import path from 'path';
 import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import WebpackMd5Hash from 'webpack-md5-hash';
+import ExtractTextPlugin from 'extract-text-webpack-plugin'
 
 export default {
     debug: true,
@@ -14,9 +16,15 @@ export default {
     output: {
         path: path.resolve(__dirname, 'dist'),
         publicPath: '/',
-        filename: '[name].js'
+        filename: '[name].[chunkhash].js'
     },
     plugins: [
+			// Generate an external css file with a hash in the filename
+			new ExtractTextPlugin('[name].[contenthash].css'),
+
+			// Hash the files using MD5 so that their names change when the content changes.
+			new WebpackMd5Hash(),
+
 			// Use CommonsChunkPlugin to create a separate bundle
 			// of vendor libraries so that they're caches separately.
 			new webpack.optimize.CommonsChunkPlugin({
@@ -38,7 +46,10 @@ export default {
 					minifyCSS: true,
 					minifyURLs: true
 				},
-				inject: true
+				inject: true,
+				// Properties you define here are available in index.html
+				// using htmlWebpackPlugin.options.varName
+				trackJsToken: 'afcbc11b7492407582b1de89c655c29e'
 			}),
 
 			// Eliminate duplicate packages when generating bundle
@@ -50,7 +61,7 @@ export default {
     module: {
         loaders: [
             {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
-            {test: /\.css$/, loaders: ['style', 'css']}
+            {test: /\.css$/, loader: ExtractTextPlugin.extract('css?sourceMap')}
         ]
     }
 }
