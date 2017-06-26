5a5334a8c12aa9453b1d325459cf350e0e423cb1
Kenneth Ramos <kramos@eduware.com>
2017-06-26 17:52:00 -0400

    Started configs for production build & minification

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,16 +1,17 @@
 import path from 'path';
+import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 export default {
     debug: true,
-    devtool: 'inline-source-map',
+    devtool: 'source-map',
     noInfo: false,
     entry: [
         path.resolve(__dirname, 'src/index')
     ],
     target: 'web',
     output: {
-        path: path.resolve(__dirname, 'src'),
+        path: path.resolve(__dirname, 'dist'),
         publicPath: '/',
         filename: 'bundle.js'
     },
@@ -18,8 +19,26 @@ export default {
 			// Create HTML file that includes reference to bundled JS.
 			new HtmlWebpackPlugin({
 				template: 'src/index.html',
+				minify: {
+					removeComments: true,
+					collapseWhitespace: true,
+					removeRedundantAttributes: true,
+					useShortDoctype: true,
+					removeEmptyAttributes: true,
+					removeStyleLinkTypeAttributes: true,
+					keepClosingSlash: true,
+					minifyJS: true,
+					minifyCSS: true,
+					minifyURLs: true
+				},
 				inject: true
-			})
+			}),
+
+			// Eliminate duplicate packages when generating bundle
+			new webpack.optimize.DedupePlugin(),
+
+			// Minify JS
+			new webpack.optimize.UglifyJsPlugin()
 		],
     module: {
         loaders: [
