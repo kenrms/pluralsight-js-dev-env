0a9f8d282cd21572ba77c5113a71bbc45bb6be48
Ken Ramos <vonnduven@gmail.com>
2017-06-25 22:41:45 -0400

    webpack index js and css demo

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -8,9 +8,9 @@ const port = 3000;
 const app = express();
 const compiler = webpack(config);
 
-app.use(require('webpack-dev-middleware'), (compiler, {
-    noInfo: true,
-    publicPath: config.output.publicPath
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
 }));
 
 app.get('/', function(req, res) {
