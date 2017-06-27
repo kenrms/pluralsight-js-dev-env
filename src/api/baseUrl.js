98cf2ecebd1e08b27303b1da7776cbf67209deb9
Kenneth Ramos <kramos@eduware.com>
2017-06-27 16:43:53 -0400

    Changed configs to use external api hosted on heroku

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,5 +1,5 @@
 export default function getBaseUrl() {
-	return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : '/';
+	return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : 'https://calm-woodland-27750.herokuapp.com/';
 }
 
 function getQueryStringParameterByName(name, url) {
